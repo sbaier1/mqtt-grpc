ab3bf1937ffc35cbec4f1ceaae3ad1a9fcd043fa
sbaier <SimonBaier@outlook.de>
2023-05-12 18:45:26 +0200

    feat: project init

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("io.grpc:grpc-api:1.55.1")
+    implementation("com.hivemq:hivemq-mqtt-client:1.3.1")
+
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
